42e70205d1123db2032e9c957f8a365ae861829c
lyming94 <44185539@qq.com>
2021-12-18 14:34:18 +0800

    hide windows title

--- windows/window_manager.cpp
+++ windows/window_manager.cpp
@@ -485,7 +485,7 @@ void WindowManager::SetTitleBarStyle(const flutter::EncodableMap &args)
     {
 //        gwlStyle = gwlStyle & ~WS_CAPTION;
         gwlStyle = gwlStyle &~WS_CAPTION &~WS_SYSMENU &~WS_SIZEBOX;
-        gwlStyle = gwlStyle &~WS_CAPTION  &~WS_SYSMENU;
+        gwlStyle = gwlStyle &~WS_CAPTION ;
         SetWindowLong(hWnd, GWL_STYLE, gwlStyle);
     }
     else
